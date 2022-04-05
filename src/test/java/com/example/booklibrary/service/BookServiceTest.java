0c465b56cb66242101e73f580d64b74fc5075644
Florian Knoerzer <knoerzerflorian@web.de>
2022-04-05 19:02:18 +0200

    Incorrect Tests
    Tests added:
    getBookById()
    getAllBooks()

--- src/test/java/com/example/booklibrary/service/BookServiceTest.java
+++ src/test/java/com/example/booklibrary/service/BookServiceTest.java
@@ -4,6 +4,7 @@ import com.example.booklibrary.model.Book;
 import com.example.booklibrary.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,19 @@ class BookServiceTest {
     Book book1 = new Book("1234", "Bibel");
     Book book2 = new Book("666", "Black Magic");
 
+   public void fillRepo() {
+       bookRepo.addBook(book1);
+       bookRepo.addBook(book2);
+   }
+
+/*    List<Book> bookList = new ArrayList<>();
+
+    public void addBookToList () {
+        bookList.add(book1);
+        bookList.add(book2);
+    }*/
+
+
     @Test
     void addBook() {
         //GIVEN
@@ -29,20 +43,43 @@ class BookServiceTest {
         //THEN
         Book expected = book1;
 
-        verify(bookRepo).addBook(new Book("1234", "Bibel"));
+        //verify(bookRepo).addBook(new Book("1234", "Bibel"));
         assertEquals(expected,actual);
 
     }
 
     @Test
     void getAllBooks() {
+        //Given
+        when(bookRepo.getAllBooks()).thenReturn(List.of(book1,book2));
+
+        //when
+        List actual = bookService.getAllBooks();
+
+
+        //then
+        List expected = List.of(book1,book2);
+
+        assertEquals(expected, actual);
+        verify(bookRepo).getAllBooks();
     }
 
     @Test
     void getBookById() {
+        //Given
+        when(bookRepo.getBookById("1234")).thenReturn(book1);
+
+        //when
+        Book actual = bookService.getBookById("1234");
+
+        //Then
+        Book expected = book1;
+        //verify(bookRepo).getBookById("1234");
+        assertEquals(expected, actual);
     }
 
     @Test
     void deleteBook() {
+
     }
 }
